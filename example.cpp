0c7c19f64924a2cb5915b6406647f44cceb77ce3
Rayyah Ajmal <rayyahajmal@gmail.com>
2025-05-06 23:59:38 +0530

    Add files via upload

---
+++ example.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+    int studentAge = 23;
+    float studentFee = 75.25;
+    char studentGrade = 'B';
+
+    cout<< "Student Age:"<<studentAge<< "\n";
+    cout<<  "Student Fee:"<<studentFee<< "\n";
+    cout<< "Student Grade:"<<studentGrade<< "\n";
+}
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int x ,y;
+int main(){
+    int sum;
+    cout<<"Type a number:";
+    cin>>x;
+    cout<<"Typr another number:";
+    cin>>y;
+    sum=x+y;
+    cout<<"Sum is:"<<sum;
+}
